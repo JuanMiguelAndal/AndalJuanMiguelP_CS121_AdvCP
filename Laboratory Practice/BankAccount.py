85456a3c13adc5cb07dfad56201664bbf049c6ed
Miguel <24-03699@g.batstate-u.edu.ph>
2025-05-15 01:05:52 +0800

    Update BankAccount.py

--- Laboratory Practice/BankAccount.py
+++ Laboratory Practice/BankAccount.py
@@ -1,62 +1,81 @@
-class BankAccount:
-    def _init_(self, account_number, balance):
-        self.account_number = account_number
-        self.balance = balance
+from abc import ABC, abstractmethod
 
+class BankAccount(ABC):
+    def __init__(self, account_number, balance=0):
+        self._account_number = account_number
+        self._balance = balance\
+        
     @property
     def account_number(self):
-        return self.account_number
+        return self._account_number
     
     @property
     def balance(self):
-        return self.balance
+        return self._balance
     
-    def deposit(amount):
+    @abstractmethod
+    def deposit(self, amount):
         pass
 
-    def withdraw(ammount):
+    @abstractmethod
+    def withdraw(self, amount):
         pass
 
+    @abstractmethod
     def display_account_type():
         pass
+
 
 class CurrentAccount(BankAccount):
-    Overdraft_limit = -5000
+    limit = -5000
+
     def deposit(self, amount):
-        self.balance += amount
+        self._balance += amount
+        print(f"DEPOSITED: {amount}! CURRENT BALANCE: {self.balance}! ACC NO. {self.account_number}")
 
     def withdraw(self, amount):
-        if self.balance - amount >= self.Overdraft_limit:
-            self.balance -= amount
-
-    def display_account_type():
+        if self._balance - amount >= self.limit:
+            self._balance -= amount
+            print(f"WITHDRAWING: {amount}! CURRENT BALANCE: {self.balance}! ACC NO. {self.account_number}")
+        else:
+            print("UNABLE TO WITHDRAW")
+    
+    def display_account_type(self):
         return "Current Account"
     
-class SavingsAccount(BankAccount):
+class SavingAccount(BankAccount):
     def deposit(self, amount):
-        self.balance += amount
-
+        self._balance += amount
+        print(f"DEPOSITED: {amount}! CURRENT BALANCE: {self.balance}! ACC NO. {self.account_number}")
+    
     def withdraw(self, amount):
-        self.balance -= amount
-
-
-    def display_account_type():
+        if self._balance >= amount:
+            self._balance -= amount
+            print(f"WITHDRAWING: {amount}! CURRENT BALANCE: {self.balance}! ACC NO. {self.account_number}")
+        else:
+            print("UNABLE TO WITHDRAW")
+    
+    def display_account_type(self):
         return "Savings Account"
 
 def print_account_details(account):
-    print("Account Number: ", account.account_number)
-    print("Balance: ", account.balance)
-    print("Type: ", account.account_type)
+    print("----------------------------------------------------------------")
+    print(f"Account Number: {account.account_number}")
+    print(f"Balance: {account.balance}")
+    print(f"Type: {account.display_account_type()}")
+    print("----------------------------------------------------------------")
 
-if __name__ == "__main__":
-    sa1 = SavingsAccount()
-  
+save1 = SavingAccount("SA123", 1200)
+save2 = SavingAccount("SA321", 15000)
+save2.deposit(1000)
+save2.withdraw(200)
 
-    sa1.account_number("WOW")
-    sa1.balance(10000)
-    sa1.deposit(100)
-    sa1.withdraw(300)
-    sa1.display_account_type
+current1 = CurrentAccount("CA456", -200)
+current2 = CurrentAccount("CA654", 500)
+current2.withdraw(800)
+current2.deposit(200)
 
-    print_account_details(sa1)
+accounts = [save1, save2, current1, current2]
+for acc in accounts:
+    print_account_details(acc)
     
